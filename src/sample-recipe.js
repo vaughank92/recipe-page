d5e61e33bf35914a32f17bad8eeaaf86f0fad809
Katie Vaughan <vaughank92@gmail.com>
2019-10-29 13:56:06 -0400

    adding sample chocolate chip recipe, still working on potential structure to eventually move to a db

---
+++ src/sample-recipe.js
@@ -0,0 +1,35 @@
+const recipe = {
+    chocolatechip: {
+      name: "Chocolate Chip Cookies",
+      image: "",
+      desc: "Nestle Tollhouse Chocolate Chip Cookies",
+      source: "https://www.allrecipes.com/recipe/174864/original-nestle-toll-house-chocolate-chip-cookies/",
+      keywords: [
+        "dessert",
+        "cookies",
+        "chocolate",
+        "baking"
+      ],
+      ingredients: {
+          "all-purpose flour" : "2 1/4 - cups",
+          "baking soda" : "1 - teaspoon",
+          "salt" : "1 - teaspoon",
+          "butter" : "1 - cup",
+          "sugar" : "3/4 - cup",
+          "brown sugar" : "3/4 - cup",
+          "vanilla extract" : "1 - teaspoon",
+          "egg" : "2",
+          "semi-sweet chocolate morsels" : "2 cups"
+      },
+      directions: [
+          "Combine flour, baking soda and salt in small bowl.",
+          "Beat butter, granulated sugar, brown sugar and vanilla extract in large mixer bowl until creamy. ",
+          "Add eggs, one at a time, beating well after each addition.",
+          "Gradually beat in flour mixture.",
+          "Stir in morsels.",
+          "Drop by rounded tablespoon onto ungreased baking sheets.",
+          "Bake for 9 to 11 minutes until golden brown.",
+          "Cool on baking sheets for 2 minutes; remove to wire racks to cool completely."
+      ]
+    }
+  }
