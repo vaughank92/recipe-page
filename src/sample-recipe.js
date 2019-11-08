044721726757602c20ca67021e1a8d826e2fae19
Katie Vaughan <vaughank92@gmail.com>
2019-11-08 16:08:21 -0500

    Adding second duplicate recipe for testing purposes

--- src/sample-recipe.js
+++ src/sample-recipe.js
@@ -1,4 +1,4 @@
-const recipe = [
+const recipes = [
     {
       name: "Chocolate Chip Cookies",
       image: "",
@@ -67,5 +67,76 @@ const recipe = [
           "Bake for 9 to 11 minutes until golden brown.",
           "Cool on baking sheets for 2 minutes; remove to wire racks to cool completely."
       ]
+    },
+    {
+        name: "Duplicated Chocolate Chip Cookies",
+        image: "",
+        desc: "Nestle Tollhouse Chocolate Chip Cookies",
+        source: "https://www.allrecipes.com/recipe/174864/original-nestle-toll-house-chocolate-chip-cookies/",
+        keywords: [
+          "dessert",
+          "cookies",
+          "chocolate",
+          "baking"
+        ],
+        ingredients: [
+          {
+              ingredient: "all-purpose flour",
+              amount: "2.25",
+              measure: "cups"
+          },
+          {
+              ingredient: "baking soda",
+              amount: "1",
+              measure: "teaspoon"
+          },
+          {
+              ingredient: "salt",
+              amount: "1",
+              measure: "teaspoon"
+          },
+          {
+              ingredient: "butter",
+              amount: "1",
+              measure: "cup"
+          },
+          {
+              ingredient: "sugar",
+              amount: ".75",
+              measure: "cup"
+          },
+          {
+              ingredient: "brown sugar",
+              amount: ".75",
+              measure: "cup"
+          },
+          {
+              ingredient: "vanilla extract",
+              amount: "1",
+              measure: "teaspoon"
+          },
+          {
+              ingredient: "egg",
+              amount: "2",
+              measure: ""
+          },
+          {
+              ingredient: "semi-sweet chocolate morsels",
+              amount: "2",
+              measure: "cups"
+          }
+        ],
+        directions: [
+            "Combine flour, baking soda and salt in small bowl.",
+            "Beat butter, granulated sugar, brown sugar and vanilla extract in large mixer bowl until creamy. ",
+            "Add eggs, one at a time, beating well after each addition.",
+            "Gradually beat in flour mixture.",
+            "Stir in morsels.",
+            "Drop by rounded tablespoon onto ungreased baking sheets.",
+            "Bake for 9 to 11 minutes until golden brown.",
+            "Cool on baking sheets for 2 minutes; remove to wire racks to cool completely."
+        ]
     }
 ]
+
+export default recipes;
