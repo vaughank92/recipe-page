3e55cae5f214cd76da1b21f9953137e381224a52
Katie Vaughan <vaughank92@gmail.com>
2019-11-08 16:08:45 -0500

    Updating routes for recipes homepage

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFound from './NotFound';
 import App from './App';
+import RecipeContainer from './RecipeContainer';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App}/>
+      <Route exact path="/recipe" component={RecipeContainer}/>
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
