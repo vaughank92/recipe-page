f7ec80f22bd447313965cc7c9ced6cafa1002668
Katie Vaughan <vaughank92@gmail.com>
2019-11-18 15:51:16 -0500

    listing directions vs paragraph

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Directions = (props) => (
-  <div className="directions">{props.directionsList}</div>
+  <div className="directions">
+    <h4>Directions</h4>
+    {props.directionsList.map((item) => {
+      return <ol>{item}</ol>
+    })}
+  </div>
 )
 
 export default Directions;
