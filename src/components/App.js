4e83122b45ad41f7f8b07d694e6da92bb029547e
Katie Vaughan <vaughank92@gmail.com>
2020-01-08 14:57:08 -0500

    migrating from class component to be able to utilize hooks for fetching from the api

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import sampleRecipes from '../sample-recipe';
 import Header from './Header';
 import '../css/styles.css';
 
-class App extends React.Component {
+function App() {
 
-  apiCheck() {
+  useEffect(() => {
     fetch("http://localhost:7777/api/recipes")
         .then(res => res.json())
         .then(data => console.log(data))
         .catch(e => console.error(e))
-  }
+  });
 
-  state = {
-    recipes: sampleRecipes,
-  };
-
-  componentDidMount() {
-    this.apiCheck();
-  }
-
-  render() {
-
-    return (
-      <div className="appComponent">
-        <Header />
-      </div>
-    )
-  }
+  return (
+    <div className="appComponent">
+      <Header />
+    </div>
+  )
 }
-
 
 export default App;
