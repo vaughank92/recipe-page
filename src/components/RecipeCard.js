5df747398e70d9203437bd8ac0bc5b8178c79336
Katie Vaughan <vaughank92@gmail.com>
2020-02-03 12:18:04 -0500

    adding info to the recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-
 const RecipeCard = (props) => {
     const name = props.name;
     const goToRecipe = props.recipeClick;
+    console.log(props);
     return (
         <div id={props.details.url} className="recipeCard" onClick={goToRecipe}>
-            <span>{name}</span>
+            <span className="recipeTitle">{name}</span>
+            <div className="recipeDesc">
+                {props.details.desc}
+            </div>
+            <div className="recipeKeys">
+                {props.details.keywords}
+            </div>
         </div>
 
     )
