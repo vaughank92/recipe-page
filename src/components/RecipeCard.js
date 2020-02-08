ca21366233b6a65caad3d20b5c06d7da08a42ff2
Katie Vaughan <vaughank92@gmail.com>
2020-02-08 00:18:05 -0500

    moving map to outside the return

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,6 +4,9 @@ const RecipeCard = (props) => {
     const name = props.name;
     const goToRecipe = props.recipeClick;
     const keywords = props.details.keywords;
+
+    let keyDom = keywords.map((word, index) => {
+        return <span key={index}>{word} </span> })
     return (
         <div id={props.details.url} className="recipeCard" onClick={goToRecipe}>
             <span className="recipeTitle">{name}</span>
@@ -11,9 +14,7 @@ const RecipeCard = (props) => {
                 {props.details.desc}
             </div>
             <div className="recipeKeys">
-                { keywords.map((word, index) => {
-                    return <span key={index}>{word} </span>
-                })}
+                { keyDom }
             </div>
         </div>
 
