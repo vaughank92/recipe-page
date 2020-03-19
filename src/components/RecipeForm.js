51972c80678e96991b23cb93057075109404c565
Katie Vaughan <vaughank92@gmail.com>
2020-03-19 18:41:15 -0400

    Updating form to update state as keys are pressed saving the fields to the state

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,48 +1,82 @@
 import React from 'react';
+import { useReducer } from 'react';
 
 const RecipeForm = () => {
+
+    const initialState = {
+        title: '',
+        source: '',
+        desc: '',
+        keywords: '',
+        ingredients: '',
+        directions: ''
+    }
+
+    const reducer = (state, {field, value}) => {
+        return {
+            ...state,
+            [field]: value
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    console.log('state', state)
+    const { title, source, desc, keywords, ingredients, directions } = state;
+    
+    const onChange = (event) => {
+        console.log(event.target.name);
+        console.log(event.target.value);
+        dispatch({field: event.target.name, value: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target);
+    }
+    
     return (
-        <form className="recipeForm">
+        <form className="recipeForm" onSubmit={handleSubmit}>
             <h2>New Recipe Form</h2>
-            <div className="name">
-                <label for="recipeName">
+            <div className="name title">
+                <label>
                     Title
                 </label>
-                <input id="recipeName" className="recipeTitle"></input>
+                <input name='title' value={title} id="recipeName" className="recipeName" onChange={onChange}></input>
             </div>
             <div className="source">
-                <label for="recipeSource">
+                <label>
                     Source Link
                 </label>
-                <input id="recipeSource" className="recipeSource"></input>
+                <input name="source" value={source} id="recipeSource" className="recipeSource" onChange={onChange}></input>
             </div>
             <div className="description">
-                <label for="recipeDesc">
+                <label>
                     Description
                 </label>
-                <textarea id="recipeDesc" className="recipeDesc"
-                placeholder="General description, serving count, and other relevant information"></textarea>
+                <textarea name="desc" id="recipeDesc" className="recipeDesc" value={desc}
+                placeholder="General description, serving count, and other relevant information" onChange={onChange}></textarea>
             </div>
             <div className="keywords">
-                <label for="recipeKeywords">
+                <label>
                     Keywords
                 </label>
-                <input id="recipeKeywords" className="recipeKeywords"></input>
+                <input name="keywords" value={keywords} id="recipeKeywords" className="recipeKeywords" onChange={onChange}></input>
             </div>
             <div className="ingredientsList">
-                <label for="recipeIngredients">
+                <label>
                     Ingredients
                 </label>
-                <textarea id="recipeIngredients" className="recipeIngredients"
-                    placeholder="Place each ingredient on a new line">
+                <textarea name="ingredients" value={ingredients} id="recipeIngredients" className="recipeIngredients"
+                    placeholder="Place each ingredient on a new line" onChange={onChange}>
                 </textarea>
             </div>
             <div className="directions">
-                <label for="recipeDirections">
+                <label>
                     Directions
                 </label>
-                <textarea id="recipeDirections" className="recipeDirections"
-                    placeholder="Place each step on a new line">
+                <textarea name="directions" value={directions} id="recipeDirections" className="recipeDirections"
+                    placeholder="Place each step on a new line" onChange={onChange}>
                 </textarea>
             </div>
             <button type="submit" className="submit submitRecipe">Submit</button>
